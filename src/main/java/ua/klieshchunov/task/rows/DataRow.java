22fc63f48de25a1fbdabf861b861be716dcf627b
Alexey Klieshchunov <isengrym121@gmail.com>
2022-08-03 19:53:38 +0300

    Implementation of DataRow and JoinedDataRow classes

--- src/main/java/ua/klieshchunov/task/rows/DataRow.java
+++ src/main/java/ua/klieshchunov/task/rows/DataRow.java
@@ -3,4 +3,9 @@ package ua.klieshchunov.task.rows;
 public class DataRow <K, V> {
     public K key;
     public V value;
+
+    public DataRow(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
 }
--- src/main/java/ua/klieshchunov/task/rows/JoinedDataRow.java
+++ src/main/java/ua/klieshchunov/task/rows/JoinedDataRow.java
@@ -1,7 +1,37 @@
 package ua.klieshchunov.task.rows;
 
+import java.util.Objects;
+
 public class JoinedDataRow <K, V1, V2> {
     public K key;
     public V1 firstValue;
     public V2 secondValue;
+
+    public JoinedDataRow(K key, V1 firstValue, V2 secondValue) {
+        this.key = key;
+        this.firstValue = firstValue;
+        this.secondValue = secondValue;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        JoinedDataRow<?, ?, ?> that = (JoinedDataRow<?, ?, ?>) o;
+        return key.equals(that.key) && Objects.equals(firstValue, that.firstValue) && Objects.equals(secondValue, that.secondValue);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(key, firstValue, secondValue);
+    }
+
+    @Override
+    public String toString() {
+        return "JoinedDataRow{" +
+                "key=" + key +
+                ", firstValue=" + firstValue +
+                ", secondValue=" + secondValue +
+                '}';
+    }
 }
