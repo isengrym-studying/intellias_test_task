19b0f5e5d0651ea87af10300fadcacf05434a528
Alexey Klieshchunov <isengrym121@gmail.com>
2022-08-04 12:31:21 +0300

    Added equals(), hashCode() and toString() method to DataRow class

--- src/main/java/ua/klieshchunov/task/rows/DataRow.java
+++ src/main/java/ua/klieshchunov/task/rows/DataRow.java
@@ -1,5 +1,7 @@
 package ua.klieshchunov.task.rows;
 
+import java.util.Objects;
+
 public class DataRow <K, V> {
     public K key;
     public V value;
@@ -7,5 +9,26 @@ public class DataRow <K, V> {
     public DataRow(K key, V value) {
         this.key = key;
         this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DataRow<?, ?> dataRow = (DataRow<?, ?>) o;
+        return key.equals(dataRow.key) && Objects.equals(value, dataRow.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(key, value);
+    }
+
+    @Override
+    public String toString() {
+        return "DataRow{" +
+                "key=" + key +
+                ", value=" + value +
+                '}';
     }
 }
